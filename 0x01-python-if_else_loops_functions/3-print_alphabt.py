5959e92c6f4e60302e24b52a14a939cc4e15dc0c
kiage <123022606+roviae@users.noreply.github.com>
2023-08-11 11:50:23 +0300

    formatting correction number 3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for letter in range(97, 123):
     if (letter != 101) & (letter != 113):
-        print(f"{chr(letter)}", end="")
+        print("{}".format(chr(letter)), end="")
